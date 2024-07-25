b88b56cfe99ea478ac6b2758722de24d2ab7d27c
darui3018823 <151906210+darui3018823@users.noreply.github.com>
2024-07-26 07:55:25 +0900

    Add files via upload

--- js.js
+++ js.js
@@ -18,3 +18,14 @@ document.addEventListener('DOMContentLoaded', () => {
         console.error('Element with class "welcome-subtitle" not found.');
     }
 });
+
+const hamburgerMenu = document.getElementById('hamburger-menu');
+const menu = document.getElementById('menu');
+
+hamburgerMenu.addEventListener('click', () => {
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
+    }
+});
