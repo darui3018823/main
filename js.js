5c72157e67ffa8216626589d0a801bdfc90be3a6
darui3018823 <151906210+darui3018823@users.noreply.github.com>
2024-07-26 06:54:34 +0900

    Add files via upload

--- js.js
+++ js.js
@@ -2,5 +2,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const welcomeMessage = document.querySelector('.welcome-message');
     setTimeout(() => {
         welcomeMessage.classList.add('show');
-    }, 100); 
+    }, 100); //100ms
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const welcomeHeading = document.getElementById('welcome-subtitle');
+    setTimeout(() => {
+        welcomeHeading.classList.add('show');
+    }, 125); // 125ms
 });
