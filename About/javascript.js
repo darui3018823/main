f9b371a6f4a907c4e33c8f492014bbba6379f9e2
darui3018823 <151906210+darui3018823@users.noreply.github.com>
2024-07-27 00:54:05 +0900

    Add files via upload

--- About/javascript.js
+++ About/javascript.js
@@ -1,18 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const pageTitle = document.getElementById('page-title');
-    const pageContent = document.getElementById('page-content');
+    const loadingScreen = document.getElementById('loading-screen');
+    
+    setTimeout(() => {
+        loadingScreen.style.display = 'none';
+    }, 1000);
 
-    if (pageTitle && pageContent) {
-        fetch('../page1.json')
-            .then(response => response.json())
-            .then(data => {
-                pageTitle.textContent = data["1"];
-                pageContent.textContent = data["2"];
-                pageContent.textContent = data["3"];
-                pageTitle.classList.add('animate');
-                pageContent.classList.add('animate');
-                pageContent.classList.add('animate');
-            })
-            .catch(error => console.error('Error loading JSON:', error));
-    }
+    const hamburgerMenu = document.getElementById('hamburger-menu');
+    const menu = document.getElementById('menu');
+    
+    hamburgerMenu.addEventListener('click', () => {
+        menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+    });
 });
