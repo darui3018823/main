f79ce96ad52d8e1c6c34c53e171810a849b5224a
darui3018823 <151906210+darui3018823@users.noreply.github.com>
2024-07-27 15:32:20 +0900

    Update js.js

--- About/js.js
+++ About/js.js
@@ -1,10 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const loadingScreen = document.getElementById('loading-screen');
-    
-    setTimeout(() => {
-        loadingScreen.style.display = 'none';
-    }, 1000);
-
     const hamburgerMenu = document.getElementById('hamburger-menu');
     const menu = document.getElementById('menu');
     
