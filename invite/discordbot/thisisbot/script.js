d844d0dc19c0fc5729681742767dbf09b44ace1b
darui3018823 <151906210+darui3018823@users.noreply.github.com>
2024-08-29 03:25:52 +0900

    Add files via upload

--- invite/discordbot/thisisbot/script.js
+++ invite/discordbot/thisisbot/script.js
@@ -1,24 +1,49 @@
+const termsButton = document.getElementById('termsButton');
+const privacyButton = document.getElementById('privacyButton');
 const agreeCheckbox = document.getElementById('agreeCheckbox');
-const permissions = document.querySelectorAll('input[name="permissions"]');
 const inviteButton = document.getElementById('inviteButton');
 const warning = document.getElementById('warning');
 const confirmCheckbox = document.getElementById('confirmCheckbox');
 const confirmButton = document.getElementById('confirmButton');
+const minimalButton = document.getElementById('minimalButton');
+const adminButton = document.getElementById('adminButton');
+const infoMessage = document.getElementById('infoMessage');
+
+termsButton.addEventListener('click', function() {
+    window.open('https://drive.google.com/file/d/1SO5O6sEyU2k53H_8W9mvPDJMKdIZCvK1/view', '_blank');
+});
+
+privacyButton.addEventListener('click', function() {
+    window.open('https://drive.google.com/file/d/1c6oosnUhk66WX4ciZJ6hkd9crsdvneJb/view', '_blank');
+});
 
 agreeCheckbox.addEventListener('change', function() {
-    permissions.forEach(permission => {
-        permission.disabled = !this.checked;
-    });
-    inviteButton.disabled = !this.checked;
-    inviteButton.classList.toggle('disabled', !this.checked);
+    const isChecked = this.checked;
+    minimalButton.disabled = !isChecked;
+    adminButton.disabled = !isChecked;
+    inviteButton.disabled = !isChecked;
+    inviteButton.classList.toggle('disabled', !isChecked);
+    infoMessage.style.display = isChecked ? 'block' : 'none';
+});
+
+minimalButton.addEventListener('click', function() {
+    this.classList.add('selected');
+    adminButton.classList.remove('selected');
+    warning.style.display = 'block';
+});
+
+adminButton.addEventListener('click', function() {
+    this.classList.add('selected');
+    minimalButton.classList.remove('selected');
+    warning.style.display = 'none';
 });
 
 inviteButton.addEventListener('click', function() {
-    let selectedPermission = document.querySelector('input[name="permissions"]:checked');
-    if (selectedPermission && selectedPermission.value === 'minimal') {
+    let selectedPermission = document.querySelector('.permission-btn.selected');
+    if (selectedPermission && selectedPermission.id === 'minimalButton') {
         warning.style.display = 'block';
-    } else if (selectedPermission && selectedPermission.value === 'admin') {
-        window.location.href = 'https://discord.com/oauth2/authorize?client_id=1240343650664185886&permissions=8&integration_type=0&scope=bot';
+    } else if (selectedPermission && selectedPermission.id === 'adminButton') {
+        window.location.href = 'https://discord.com/oauth2/authorize?client_id=YOUR_BOT_ID&permissions=8&scope=bot';
     }
 });
 
@@ -28,5 +53,5 @@ confirmCheckbox.addEventListener('change', function() {
 });
 
 confirmButton.addEventListener('click', function() {
-    window.location.href = 'https://discord.com/oauth2/authorize?client_id=1240343650664185886&permissions=1126984386476096&integration_type=0&scope=bot';
+    window.location.href = 'https://discord.com/oauth2/authorize?client_id=YOUR_BOT_ID&permissions=2048&scope=bot';
 });
