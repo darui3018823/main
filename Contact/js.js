31e8a806cca485f2c7aeccec6ef13bd58695f126
darui3018823 <151906210+darui3018823@users.noreply.github.com>
2024-09-20 20:36:52 +0900

    Update js.js

--- Contact/js.js
+++ Contact/js.js
@@ -1,6 +1,5 @@
 const hamburgerMenu = document.getElementById('hamburger-menu');
 const menu = document.getElementById('menu');
-
 hamburgerMenu.addEventListener('click', () => {
     if (menu.style.display === 'block') {
         menu.style.display = 'none';
