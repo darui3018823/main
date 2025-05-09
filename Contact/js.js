9832177a4d712e00f18f21dcfbd8ccecb5ab997e
darui3018823 <n.sumikko.0711@gmail.com>
2025-05-09 15:52:17 +0900

    contactの改修v1

--- Contact/js.js
+++
@@ -1,9 +0,0 @@
-const hamburgerMenu = document.getElementById('hamburger-menu');
-const menu = document.getElementById('menu');
-hamburgerMenu.addEventListener('click', () => {
-    if (menu.style.display === 'block') {
-        menu.style.display = 'none';
-    } else {
-        menu.style.display = 'block';
-    }
-});
