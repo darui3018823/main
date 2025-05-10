6b0c8d4d475e41f688eec981624d16180a95c67d
darui3018823 <n.sumikko.0711@gmail.com>
2025-05-11 01:07:35 +0900

    Update: Js Files

--- links/script.js
+++ links/script.js
@@ -1,11 +1,14 @@
-const hamburger = document.getElementById('hamburger');
-const menuModal = document.getElementById('menuModal');
-const closeMenu = document.getElementById('closeMenuModal');
+window.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.getElementById('hamburger');
+    const menuModal = document.getElementById('menuModal');
+    const closeMenu = document.getElementById('closeMenuModal');
 
-hamburger?.addEventListener('click', () => {
-  menuModal.classList.remove('hidden');
-});
+    // モバイルメニュー：モーダル表示切り替え
+    hamburger?.addEventListener('click', () => {
+        menuModal?.classList.remove('hidden');
+    });
 
-closeMenu?.addEventListener('click', () => {
-  menuModal.classList.add('hidden');
+    closeMenu?.addEventListener('click', () => {
+        menuModal?.classList.add('hidden');
+    });
 });
