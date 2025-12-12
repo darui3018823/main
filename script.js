cc7dd15a425a7cc5056a6eb4a631aa3daec52f92
darui3018823 <151906210+darui3018823@users.noreply.github.com>
2025-12-12 22:57:06 +0900

    feat: Integrate Tailwind CSS for UI styling.

--- script.js
+++ script.js
@@ -1,18 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
-    const title = document.getElementById('mainTitle');
-    const subtitle = document.getElementById('subTitle');
     const hamburger = document.getElementById('hamburger');
     const menuModal = document.getElementById('menuModal');
     const closeMenu = document.getElementById('closeMenuModal');
-
-    // アニメーション表示処理
-    setTimeout(() => {
-        title?.classList.remove('opacity-0', 'translate-y-8');
-    }, 100);
-
-    setTimeout(() => {
-        subtitle?.classList.remove('opacity-0', 'translate-y-8');
-    }, 700);
 
     // モバイルメニュー：モーダル表示切り替え
     hamburger?.addEventListener('click', () => {
